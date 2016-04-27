67393a3a2fb1fdcffd86311df86acd33e0de805c
root <eugene.ekonomov@gmail.com>
2016-04-27 11:33:17 -0400

    Initial commit

---
+++ src/main/java/ru/ekonomov/web/HomeController.java
@@ -0,0 +1,22 @@
+package ru.ekonomov.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController
+{
+    @RequestMapping(method= RequestMethod.GET)
+    public String home()
+    {
+        return "home";
+    }
+
+    @RequestMapping(value= "/nohome", method= RequestMethod.GET)
+    public String nohome()
+    {
+        return "nohome";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/nohome.jsp
@@ -0,0 +1,9 @@
+<%@ page session="false" %>
+<html>
+<head>
+    <title>Spittr</title>
+</head>
+<body>
+text text text
+</body>
+</html>
