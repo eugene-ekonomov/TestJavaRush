7188dff6eaca06e224f974d699edc66bee960ada
eugene-ekonomov <eugene.ekonomov@gmail.com>
2016-05-19 15:36:51 +0500

    2016-05-19-15-35 (Релиз)

--- src/test/java/ru/ekonomov/web/HomeControllerTest.java
+++ src/test/java/ru/ekonomov/web/HomeControllerTest.java
@@ -2,21 +2,8 @@ package ru.ekonomov.web;
 
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.servlet.view.InternalResourceView;
-import ru.ekonomov.data.User;
-import ru.ekonomov.data.UserRepository;
-import ru.ekonomov.service.UserService;
-import ru.ekonomov.service.UserServiceImpl;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.Matchers.hasItems;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
@@ -33,40 +20,5 @@ public class HomeControllerTest
     }
 
 
-    /*@Test
-    public void testService() throws Exception {
-        List<User> expectedUsers = createUsersList(20);
-        UserRepository mockRepository =
-                mock(UserRepository.class);
-        when(mockRepository.findByName("Vasya1"))
-                .thenReturn(expectedUsers);
 
-        UserServiceImpl service = new UserServiceImpl();
-        service.userRepository = mockRepository;
-
-        HomeController controller = new HomeController();
-        controller.userService = service;
-
-        MockMvc mockMvc = standaloneSetup(controller)
-                .setSingleView(
-                        new InternalResourceView("/WEB-INF/views/finduserbyid.jsp"))
-                .build();
-        mockMvc.perform(post("/finduserbyid","id=0"))
-                .andExpect(view().name("finduserbyid"))
-                .andExpect(model().attributeExists("user"))
-                .andExpect(model().attribute("user",
-                        hasItems(expectedUsers.toArray())));
-    }
-    private List<User> createUsersList(int count)
-    {
-        List<User> users = new ArrayList<User>();
-        for (int i=0; i < count; i++)
-        {
-            User user = new User();
-            user.setName("Vasya" + i);
-            user.setAge(i);
-            users.add(user);
-        }
-        return users;
-    }*/
 }
